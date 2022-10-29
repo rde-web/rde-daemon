029468128df07a4896fbc97ef34f4545e53cfff5
Калюжный Алексей <abkalyuzhn@mts.ru>
2022-10-29 14:26:14 +0300

    dev

--- cmd/rde-daemon/main.go
+++ cmd/rde-daemon/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"errors"
+	"io"
 	"log"
 	"net"
 
@@ -11,13 +13,68 @@ import (
 )
 
 func main() {
-	lis, err := net.Listen("tcp", ":9090")
+	const daemonAddr string = "localhost:46046"
+	lis, err := net.Listen("tcp", daemonAddr)
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
-	var opts []grpc.ServerOption
+	defer lis.Close()
+	var opts []grpc.ServerOption = make([]grpc.ServerOption, 0)
+	opts = append(opts)
 
 	grpcServer := grpc.NewServer(opts...)
 	RegisterRDEFSServer(grpcServer, FS{})
-	grpcServer.Serve(lis)
+	go grpcServer.Serve(lis)
+
+	proxyLis, err := net.Listen("tcp", ":9090")
+	if err != nil {
+		log.Fatalf("failed to listen PROXY: %v", err)
+	}
+	defer proxyLis.Close()
+	for {
+		conn, err := proxyLis.Accept()
+		if err != nil {
+			println("err accept PROXY", err.Error())
+			break
+		}
+		sock, err := net.Dial("tcp", daemonAddr)
+		if err != nil {
+			println("err dial DAEMON", err.Error())
+			break
+		}
+		for {
+			var buffp []byte = make([]byte, 1024)
+			nr, err := conn.Read(buffp)
+			if err != nil {
+				if errors.Is(err, io.EOF) {
+					continue
+				}
+				println("err read PROXY", err.Error())
+				break
+			}
+			println("readed PRXOY", nr, string(buffp))
+
+			if _, err := sock.Write(buffp); err != nil {
+				println("err write DAEMON", err.Error())
+				lis.Close()
+				break
+			}
+			var buffd []byte = make([]byte, 1024)
+			if nr, err = sock.Read(buffd); err != nil {
+				if errors.Is(err, io.EOF) {
+					continue
+				}
+				println("err read DAEMON", err.Error())
+				break
+			}
+			println("readed DAEMON", nr, string(buffd))
+			if _, err := conn.Write(buffd); err != nil {
+				println("err write PROXY", err.Error())
+				break
+			}
+
+		}
+		conn.Close()
+		sock.Close()
+	}
 }
